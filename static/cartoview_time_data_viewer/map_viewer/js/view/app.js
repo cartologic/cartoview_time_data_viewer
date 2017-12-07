6100708124d11d222e5ecc4aafe2989ca0d28119
hisham waleed karam <hisham.karam@cartologic.com>
2017-12-07 13:11:03 +0200

    remove dependency and cartoview proxy

---
+++ static/cartoview_time_data_viewer/map_viewer/js/view/app.js
@@ -0,0 +1,9 @@
+
+var app = angular.module('cartoview.mapViewerApp', [
+    'ngMaterial',
+    'cartoview.base',
+    'cartoview.viewer.urlsHelper',
+    'cartoview.viewer.mapConfig',
+    'cartoview.viewer.appConfig',
+    'cartoview.viewer.widgetsInfo'
+]);
